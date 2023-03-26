450e31095662bc84617a049b482652813d165deb
ivaylokarafeizov <qwerty36758@gmail.com>
2023-03-26 19:46:05 +0300

    Added contexts folder

---
+++ src/contexts/AuthContext.js
@@ -0,0 +1,78 @@
+import { createContext, useContext } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { useLocalStorage } from '../hooks/useLocalStorage';
+
+import { authServiceFactory } from '../services/authService';
+
+export const AuthContext = createContext();
+
+export const AuthProvider = ({ children }) => {
+    const [auth, setAuth] = useLocalStorage('auth', {});
+    const navigate = useNavigate();
+
+    const authService = authServiceFactory(auth.accessToken);
+
+    const onLoginSubmit = async (data) => {
+        try {
+            const result = await authService.login(data);
+
+            setAuth(result);
+
+            navigate('/');
+        } catch (error) {
+            throw Error('Error:' + error.message);
+        }
+    };
+
+    const onRegisterSubmit = async (values) => {
+        const { confirmPassword, ...registerData } = values;
+        if (!confirmPassword || !registerData) {
+            throw new Error('All fields are required');
+        }
+
+        if (confirmPassword !== registerData.password) {
+            throw new Error('Passwords do not match');
+        }
+
+        try {
+            const result = await authService.register(registerData);
+
+            setAuth(result);
+
+            navigate('/');
+        } catch (error) {
+            throw Error('Error:' + error.message);
+        }
+    };
+
+    const onLogout = async () => {
+        await authService.logout();
+
+        setAuth({});
+    };
+
+    const contextValues = {
+        onLoginSubmit,
+        onRegisterSubmit,
+        onLogout,
+        userId: auth._id,
+        token: auth.accessToken,
+        username: auth.username,
+        userEmail: auth.email,
+        isAuthenticated: !!auth.accessToken,
+    };
+
+    return (
+        <>
+            <AuthContext.Provider value={contextValues}>
+                {children}
+            </AuthContext.Provider>
+        </>
+    );
+};
+
+export const useAuthContext = () => {
+    const context = useContext(AuthContext);
+
+    return context;
+};
