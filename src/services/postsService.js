3c06d0c127e97cbf208d23f8a6c72944d11212a9
ivaylokarafeizov <qwerty36758@gmail.com>
2023-03-28 19:29:27 +0300

    Added postsService.js

---
+++ src/services/postsService.js
@@ -0,0 +1,17 @@
+let baseUrl = 'http://localhost:3030/jsonstore/space/';
+
+export const getAll = async () => {
+    const response = await fetch(baseUrl);
+
+    if (response.status === 204) {
+        return {};
+    }
+
+    const result = await response.json();
+
+    if (!response.ok) {
+        throw result;
+    }
+
+    return Object.values(result.posts);
+};
